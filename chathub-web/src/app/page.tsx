139fb6920790dbeb46cd3275604cbbf06cff30d5
Linh Ha Phuong <tuthanh375@gmail.com>
2025-01-24 13:27:54 +0700

    Redesign the UI

--- chathub-web/src/app/page.tsx
+++ chathub-web/src/app/page.tsx
@@ -1,14 +1,21 @@
+import Image from "next/image"
+import { Images } from "../constants/images"
 import ChatList from "../components/chat-list"
-import SignInPage from "./sign-in/page"
-import SignUpPage from "./sign-up/page"
-import ResetPasswordPage from "./reset-password/page"
 
 export default function Home() {
   return (
-    <div className="flex flex-row justify-between">
-      <SignInPage />
-
+    <div className="flex flex-row justify-between h-screen">
+      <ChatList />
+      <div className="flex flex-1 items-center justify-center relative">
+        <Image 
+          src={Images.Background} 
+          alt="background-image" 
+          layout="fill" 
+          objectFit="cover" 
+          className="absolute inset-0" 
+        />
+        <div className="absolute inset-0 bg-black opacity-30" />
+      </div>
     </div>
   )
 }
-
