cfb775eebd4569a38114659bb33fb95568268f07
datvct <vancongthanhdata10@gmail.com>
2025-02-26 01:26:14 +0700

    feat: integrate GetMessagesOfConversation API with Chat Area Component and fix minor bug in chat info components

--- chathub-web/src/lib/utils.ts
+++ chathub-web/src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatDateTime = (isoString: string) => {
+  const date = new Date(isoString);
+
+  const timeFormatter = new Intl.DateTimeFormat("vi-VN", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+
+  const dateFormatter = new Intl.DateTimeFormat("vi-VN", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  return `${timeFormatter.format(date)} ${dateFormatter.format(date)}`;
+};
