216860867eda942fb62b942a425b6e1d0bfaadc9
dhlananhh <21087481.anh@student.iuh.edu.vn>
2025-02-27 00:15:56 +0700

    feat: create getlib & use hook update profile

---
+++ chathub-web/src/lib/get-update-profile.ts
@@ -0,0 +1,21 @@
+// lib/get-update-profile.ts
+import { User } from "../codegen/User";
+import { ChangeProfileRequest, SuccessResponse, ErrorResponse } from "../codegen/data-contracts";
+
+const userInstance = new User({ baseUrl: process.env.API_URL });
+
+export async function updateProfile(data: ChangeProfileRequest, token: string) {
+  try {
+    if (!data.id) return null;
+    const response = await userInstance.updateProfile(data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }) as SuccessResponse;
+    return response;
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    throw error;
+    // return { errorCode: 500, message: "Lỗi cập nhật profile. Xin vui lòng thử lại."}
+  }
+}
