64339d75396e3591e9a5a0bf78c8038aef077a63
baoispro <nguyenlegiabao810@gmail.com>
2025-04-13 22:35:47 +0700

    [feature/Chat-151] Integrate unsent/delete message API

--- chathub-web/src/components/chat-message.tsx
+++ chathub-web/src/components/chat-message.tsx
@@ -7,19 +7,45 @@ import { MessageType } from "~/types/types"
 import Link from "next/link"
 import { IoIosDocument } from "react-icons/io"
 import moment from "moment"
+import { CiMenuKebab, CiFaceSmile } from "react-icons/ci"
+import { TiArrowBackOutline, TiArrowForwardOutline } from "react-icons/ti"
+import { useState } from "react"
+import {unsendMessage, deleteMessage, forwardMessage} from "~/lib/get-message"
 
 interface ChatMessageProps {
   messages: MessageResponse[]
   userId: number
   isGroupChat: boolean
   messagesEndRef: React.RefObject<HTMLDivElement>
+  token: string
+  refetchMessages: () => void
 }
 
-const ChatMessage = ({ messages, userId, isGroupChat, messagesEndRef }: ChatMessageProps) => {
+const ChatMessage = ({ messages, userId, isGroupChat, messagesEndRef, token, refetchMessages }: ChatMessageProps) => {
   let lastMessageDate: string | null = null
+  const [openMenuId, setOpenMenuId] = useState<number | null>(null)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const handleUnsend = async (messageId: number) => {
+    const res = await unsendMessage(userId, messageId, token)
+    setIsMenuOpen(false)
+  }
+  
+  const handleDelete = async (messageId: number) => {
+    const res = await deleteMessage(userId, messageId, token)
+    if(res) {
+      refetchMessages()
+    }
+  }
+  
+  // const handleForward = async (messageId: number) => {
+  //   const conversationIds = [/* ví dụ: 123, 456 */] // Tuỳ vào cách bạn chọn hội thoại để forward
+  //   const res = await forwardMessage(userId, messageId, conversationIds, localStorage.getItem("token") ?? "")
+  //   console.log(res)
+  // }  
 
   return (
-    <div className="flex flex-col space-y-3" key={messages[0]?.id}>
+    <div className="flex flex-col space-y-3 key={messages[0]?.id}">
       {messages
         .slice()
         .sort((a, b) => new Date(a.sentAt).getTime() - new Date(b.sentAt).getTime())
@@ -34,8 +60,35 @@ const ChatMessage = ({ messages, userId, isGroupChat, messagesEndRef }: ChatMess
               )}
               <div
                 id={`message-${msg.id}`}
-                className={`message flex ${msg.senderId === userId ? "justify-end" : "justify-start"}`}
+                className={`message items-center gap-2 h-[141px] ${msg.senderId === userId ? "justify-end" : "justify-start"} ${msg.userDeleted==true ? "hidden" : "flex"}`}
               >
+                {msg.senderId === userId && (
+                  <div className="flex items-center gap-2">
+                    <div className="flex items-center gap-2">
+                      <div
+                        className={`bg-[#252728] pl-3 pr-5 py-3 rounded-lg shadow-md ${openMenuId === msg.id && isMenuOpen === true ? "block" : "hidden"}`}
+                      >
+                        <ul>
+                          {msg.unsent === false && <li className="hover:bg-[#333334] p-2 pr-3 rounded cursor-pointer" onClick={() => handleUnsend(msg.id)}>Recall</li>}
+                          <li className="hover:bg-[#333334] p-2 pr-3 rounded cursor-pointer" onClick={() => handleDelete(msg.id)}>Delete Message</li>
+                          {msg.unsent === false && <li className="hover:bg-[#333334] p-2 pr-3 rounded cursor-pointer">Forward</li>}
+                        </ul>
+                      </div>
+                      <button
+                        className="hover:bg-[#333334] p-2 rounded-full realative"
+                        onClick={() => {setOpenMenuId(openMenuId === msg.id ? null : msg.id); setIsMenuOpen(!isMenuOpen)}}
+                      >
+                        <CiMenuKebab />
+                      </button>
+                    </div>
+                    <button className="hover:bg-[#333334] p-2 rounded-full">
+                      <TiArrowBackOutline />
+                    </button>
+                    <button className="hover:bg-[#333334] p-2 rounded-full">
+                      <CiFaceSmile />
+                    </button>
+                  </div>
+                )}
                 {msg.senderId !== userId && (
                   <Image
                     src={Images.AvatarDefault}
@@ -87,6 +140,17 @@ const ChatMessage = ({ messages, userId, isGroupChat, messagesEndRef }: ChatMess
                       <source src={msg.content} type="video/mp4" />
                       Your browser does not support the video tag.
                     </video>
+                  ) : msg.unsent === true ? (
+                    <p
+                      className={`p-3 rounded-lg w-max max-w-xs break-words text-black whitespace-pre-wrap ${
+                        msg.senderId === userId ? "bg-[#1566A3] text-white" : "bg-[#F0F0F0]"
+                      } ${isOnlyEmoji(msg.content) ? "text-4xl p-2 bg-transparent" : ""}`}
+                    >
+                      Message recalled
+                      <span className={`text-xs block mt-1 ${msg.senderId === userId ? "text-white" : "text-black"}`}>
+                        {formatTimeSendAt(msg.sentAt)}
+                      </span>
+                    </p>
                   ) : (
                     <p
                       className={`p-3 rounded-lg w-max max-w-xs break-words text-black whitespace-pre-wrap ${
@@ -100,6 +164,32 @@ const ChatMessage = ({ messages, userId, isGroupChat, messagesEndRef }: ChatMess
                     </p>
                   )}
                 </div>
+                {msg.senderId != userId && (
+                  <div className="flex items-center gap-2">
+                    <button className="hover:bg-[#333334] p-2 rounded-full">
+                      <CiFaceSmile />
+                    </button>
+                    <button className="hover:bg-[#333334] p-2 rounded-full">
+                      <TiArrowForwardOutline />
+                    </button>
+                    <div className="flex items-center gap-2">
+                      <button
+                        className="hover:bg-[#333334] p-2 rounded-full realative"
+                        onClick={() => {setOpenMenuId(openMenuId === msg.id ? null : msg.id); setIsMenuOpen(!isMenuOpen)}}
+                      >
+                        <CiMenuKebab />
+                      </button>
+                      <div
+                        className={`bg-[#252728] pl-3 pr-5 py-3 rounded-lg shadow-md ${openMenuId === msg.id && isMenuOpen === true ? "block" : "hidden"}`}
+                      >
+                        <ul>
+                          <li className="hover:bg-[#333334] p-2 pr-3 rounded cursor-pointer" onClick={() => handleDelete(msg.id)}>Delete Message</li>
+                          {msg.unsent === false && <li className="hover:bg-[#333334] p-2 pr-3 rounded cursor-pointer">Forward</li>}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           )
